0151f84b83468bd55380b8120be773dbaf870d25
Rm <rm@MacBook-Pro-de-Rm.local>
2019-09-05 15:06:10 -0500

    Noticias

---
+++ precache-manifest.c167ae33ebabfbab9afc5c082a91673e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f295d267ca9db29eecfa8cfc7a1afe48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ff24d41b3f30a01ff9b",
+    "url": "/static/js/2.f0d06ed8.chunk.js"
+  },
+  {
+    "revision": "1f6a0bc914dc26aaf09b",
+    "url": "/static/js/main.499075bc.chunk.js"
+  },
+  {
+    "revision": "79b1907a9121d42093b4",
+    "url": "/static/js/runtime~main.46e97f86.js"
+  }
+]);
